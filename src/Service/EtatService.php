d261d0fc944d413ae8e6a339a27c208cc9792cb5
dlrwtul <dibrahimalang@gmail.com>
2022-07-11 19:18:48 +0000

    deploy

---
+++ src/Service/EtatService.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace App\Service;
+
+class EtatService {
+
+    public const ETAT_EN_COURS = "en cours"; 
+    public const ETAT_VALIDE = "valide"; 
+    public const ETAT_EN_ATTENTE = "en attente"; 
+    public const ETAT_DISPONIBLE = "disponible";
+    public const ETAT_STOCK_EPUISE = "stock epuise";
+    public const ETAT_INDISPONIBLE = "indisponible";
+
+}
